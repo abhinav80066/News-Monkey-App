9a4c6e2321517003340961a897ee73f143fbeba9
abhinav80066 <abhinav.chaudhary800@gmail.com>
2021-11-25 17:27:50 +0530

    Added category feature and some small features

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,7 +4,7 @@ export default class NavBar extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
     <a className="navbar-brand" href="/">News Monkey</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,13 +13,28 @@ export default class NavBar extends Component {
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
+          <a className="nav-link" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">Features</a>
+          <a className="nav-link" href="/">Business</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">Pricing</a>
+          <a className="nav-link" href="/">Entertainment</a>
+        </li>
+         <li className="nav-item">
+          <a className="nav-link" href="/">General</a>
+        </li>
+         <li className="nav-item">
+          <a className="nav-link" href="/">Health</a>
+        </li>
+         <li className="nav-item">
+          <a className="nav-link" href="/">Scince</a>
+        </li>
+         <li className="nav-item">
+          <a className="nav-link" href="/">Sports</a>
+        </li>
+         <li className="nav-item">
+          <a className="nav-link" href="/">Technology</a>
         </li>
       </ul>
     </div>
