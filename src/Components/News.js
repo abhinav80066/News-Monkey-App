ffb192236720788cd68e853550ba450fc4d1372f
abhinav80066 <abhinav.chaudhary800@gmail.com>
2021-12-08 16:48:13 +0530

    Added Author Name and time in the news items

--- src/Components/News.js
+++ src/Components/News.js
@@ -66,7 +66,7 @@ export class News extends Component {
                 <div className="row">
                 {this.state.articles.map((element)=>{
                     return  <div className="col-md-4" key={element.url}>
-                    <Newsitem  title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
+                    <Newsitem  title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url}  author={element.author} date = {element.publishedAt} />
                 </div>
                 })}
                 </div>
